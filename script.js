6731106c88408f1db11f550232aefebf64334b61
Igor <igcarvalhaes@hotmail.com>
2023-05-02 23:59:31 -0300

    Add event click button & set up logic to add the squares inside the container

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 const gridContainer = document.querySelector('#grid-container');
+const btnSquarePerSide = document.querySelector('#btn-square-per-side');
 
-for (i = 0; i<800; i++){
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('grid-square');
-    gridContainer.appendChild(gridSquare);
-}
+
+const larguraGridContainer = 640;
+const alturaGridContainer = 640;
+let tamanhoGridSquare;
+let quantidadeGridSquares;
+
+btnSquarePerSide.addEventListener('click', function (tamanhoGridSquare){
+    tamanhoGridSquare = Number(prompt("Digite o tamanho dos lados do quadrado"));
+    quantidadeGridSquares = ((larguraGridContainer / tamanhoGridSquare))*((alturaGridContainer  / tamanhoGridSquare));
+
+    for (i = 0; i<quantidadeGridSquares; i++){
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
+        gridContainer.appendChild(gridSquare);
+        gridSquare.style.width = tamanhoGridSquare + "px";
+        gridSquare.style.height = tamanhoGridSquare + "px";
+    
+        gridSquare.addEventListener('mouseover', (e) => {
+            e.target.style.background = "black";
+        });
+    }
+      
+});
+
+
+
+
+/*
+quantidadeGridSquares = (larguraGridContainer) / tamanhoGridSquare) * 
+                        (alturaGridContainer  / tamanhoGridSquare);
+
+*/
