91e4ac2819391a60e3282da55adf70346c36bcd3
Igor <igcarvalhaes@hotmail.com>
2023-05-03 19:08:35 -0300

    fixed square count & width/height logic

--- script.js
+++ script.js
@@ -2,21 +2,28 @@ const gridContainer = document.querySelector('#grid-container');
 const btnSquarePerSide = document.querySelector('#btn-square-per-side');
 
 
-const larguraGridContainer = 640;
-const alturaGridContainer = 640;
+const larguraGridContainer = 500;
+const alturaGridContainer = 500;
 let tamanhoGridSquare;
 let quantidadeGridSquares;
 
+const colors = ["red", "yellow", "blue", "orange", "green", "violet", "black", "white", "gray", "tan" ,"brown"];
+const choiceColors = (Math.floor(Math.random() * colors.length));
+console.log(colors[choiceColors]);
+
 btnSquarePerSide.addEventListener('click', function (tamanhoGridSquare){
     tamanhoGridSquare = Number(prompt("Digite o tamanho dos lados do quadrado"));
-    quantidadeGridSquares = ((larguraGridContainer / tamanhoGridSquare))*((alturaGridContainer  / tamanhoGridSquare));
+    quantidadeGridSquares = tamanhoGridSquare * tamanhoGridSquare;
 
+    gridContainer.innerHTML = ""; /* limpa todo o conteúdo dentro do gridcontainer antes de adicionar os quadrados*/
+
+    
     for (i = 0; i<quantidadeGridSquares; i++){
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square');
         gridContainer.appendChild(gridSquare);
-        gridSquare.style.width = tamanhoGridSquare + "px";
-        gridSquare.style.height = tamanhoGridSquare + "px";
+        gridSquare.style.width = (larguraGridContainer/tamanhoGridSquare) + "px";
+        gridSquare.style.height = (alturaGridContainer/tamanhoGridSquare) + "px";
     
         gridSquare.addEventListener('mouseover', (e) => {
             e.target.style.background = "black";
